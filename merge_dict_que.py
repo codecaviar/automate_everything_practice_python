f11339a8bcbab37e89de3a88bafed8e6a80d73bc
codecaviar <60812709+codecaviar@users.noreply.github.com>
2020-10-17 17:01:27 -0700

    Correct practice problem docstring

--- merge_dict_que.py
+++ merge_dict_que.py
@@ -5,29 +5,27 @@
 # Fill each function stub according to the docstring.
 # Run the tests with command: "python3 -m doctest [filename].py -v"
 
-def max_lists(list1, list2):
+def merge_dict(d1, d2):
     """
-    The arguments, list1 and list2, have the same length.
-    Return a list which contains, for each index,
-    the maximum element of both list at this index.
+    Return a new dictionary which contains all the keys from d1 and d2
+    with their associated values. If a key is in both dictionaries, the
+    value should be the sum of the two values.
 
     Parameters
     ----------
-    list1: {list} of numeric values
-    list2: {list} of numeric values
+    d1: {dict} with str keys and numeric values
+    d2: {dict} with str keys and numeric values
 
     Returns
     -------
-    {list}: list of maximum values for each index of list1, list2
+    {dict}: all key and value pairs from both dictionaries
 
     Example
     -------
-    >>> max_lists([1, 4, 8], [3, 1, 9])
-    [3, 4, 9]
-    >>> max_lists([5, 7, 2, 3, 6], [3, 9, 1, 2, 8])
-    [5, 9, 2, 3, 8]
-    >>> max_lists([6, 7, 8, 9, 10], [11, 12, 13, 14, 15])
-    [11, 12, 13, 14, 15]
+    >>> d1 = {"a": 1, "b": 5, "c": 1, "e": 8}
+    >>> d2 = {"b": 2, "c": 5, "d": 10, "f": 6}
+    >>> merge_dict(d1,d2) == {"a": 1, "b": 7, "c": 6, "d": 10, "e": 8, "f": 6}
+    True
     """
 
     # Optional: use try...except block to handle error conditions
