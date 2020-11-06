59dadf1c6869d523eea45ca72f62e85e0bce9ec9
codecaviar <60812709+codecaviar@users.noreply.github.com>
2020-11-05 22:01:36 -0800

    Upload new practice problem

---
+++ array_plus_ans.py
@@ -0,0 +1,42 @@
+# Automate Everything from @codecaviar
+# Level: Basic
+# Updated: 2020-11-05
+
+# Fill each function stub according to the docstring
+# Run tests with this command: "python3 -m doctest [filename].py -v"
+
+# As you progress and problems get harder, there will be many ways of
+# producing the correct results. Keep in mind that the answers to
+# practice problems should be used as a reference, but are by no means
+# the only ways of answering the questions.
+
+def array_plus(arr1, arr2):
+    """
+    Sum all elements in two ararys
+
+    Parameters
+    ----------
+    arr1: {list} list of integers
+    arr2: {list} list of integers
+
+    Returns
+    -------
+    {int}: sum of all elements in both arrays
+
+    Example
+    -------
+    >>> array_plus([1, 2, 3], [4, 5, 6])
+    21
+    >>> array_plus([0, 0, 0], [4, 5, 6])
+    15
+    >>> array_plus([-1618, -1388, 3142, -435, -189], [780, 8135, 9546, 2027, -73])
+    19927
+    >>> array_plus([-3966, -2018, -7567, 4210, -1408], [-8779, 8062, -5359, -5381, 9977])
+    -12229
+    >>> array_plus([-2233, -1799, 7951, -7392, -6127], [9813, -7657, 204, 8854, -9432])
+    -7818
+    """
+
+    # Optional: use try...except block to handle error conditions
+
+    return sum(arr1 + arr2)
