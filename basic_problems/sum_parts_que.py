dd26fa1d1b845f559b72351ce6de76782c2457a6
codecaviar <60812709+codecaviar@users.noreply.github.com>
2020-12-01 11:36:33 -0800

    Upload new practice problem

---
+++ basic_problems/sum_parts_que.py
@@ -0,0 +1,44 @@
+# Automate Everything from @codecaviar
+# Level: Basic
+# Updated: 2020-11-28
+
+# Fill each function stub according to the docstring
+# Run tests with this command: "python3 -m doctest [filename].py -v"
+
+from functools import reduce # option to use reduce from functools 
+
+def sum_parts(input_list):
+    """
+    Example:
+    input_list = [0, 1, 3, 6, 10]
+
+    input_list = [0, 1, 3, 6, 10]
+    input_list = [1, 3, 6, 10]
+    input_list = [3, 6, 10]
+    input_list = [6, 10]
+    input_list = [10]
+    input_list = []
+
+    total_sum = [20, 20, 19, 16, 10, 0]
+
+    Parameters
+    ----------
+    input_list: {list} array of integers
+
+    Returns
+    -------
+    {list}: sum of parts as defined above
+
+    Example
+    -------
+    >>> sum_parts([1, 5, 10, 10, 2, 9, 9])
+    [46, 45, 40, 30, 20, 18, 9, 0]
+    >>> sum_parts([8, 8, 10, 6, 10, 2, 8, 7, 7])
+    [66, 58, 50, 40, 34, 24, 22, 14, 7, 0]
+    >>> sum_parts([7, 10, 8, 8, 1, 1, 3, 3])
+    [41, 34, 24, 16, 8, 7, 6, 3, 0]
+    """
+
+    # Optional: use try...except block to handle error conditions
+
+    pass # null operation, placeholder when a statement is required
