c3ac1cbb88cbe2ab385b1120153c704f44c23dc5
codecaviar <60812709+codecaviar@users.noreply.github.com>
2020-12-02 12:22:32 -0800

    Upload new practice problem

---
+++ basic_problems/multiple_table_que.py
@@ -0,0 +1,60 @@
+# Automate Everything from @codecaviar
+# Level: Basic
+# Updated: 2020-12-02
+
+# Fill each function stub according to the docstring
+# Run tests with this command: "python3 -m doctest [filename].py -v"
+
+def multiple_table(num):
+    """
+    Return multiplication table from 1 to 10 for any input integer,
+    use "\n" in string to jump to the next line
+
+    Parameters
+    ----------
+    num: {int} any number, including 0 and negtative
+
+    Returns
+    -------
+    {str}: print string of multiplcation table from 1 to 10
+
+    Example
+    -------
+    >>> multiple_table(74)
+    1 * 74 = 74
+    2 * 74 = 148
+    3 * 74 = 222
+    4 * 74 = 296
+    5 * 74 = 370
+    6 * 74 = 444
+    7 * 74 = 518
+    8 * 74 = 592
+    9 * 74 = 666
+    10 * 74 = 740
+    >>> multiple_table(2)
+    1 * 2 = 2
+    2 * 2 = 4
+    3 * 2 = 6
+    4 * 2 = 8
+    5 * 2 = 10
+    6 * 2 = 12
+    7 * 2 = 14
+    8 * 2 = 16
+    9 * 2 = 18
+    10 * 2 = 20
+    >>> multiple_table(-25)
+    1 * -25 = -25
+    2 * -25 = -50
+    3 * -25 = -75
+    4 * -25 = -100
+    5 * -25 = -125
+    6 * -25 = -150
+    7 * -25 = -175
+    8 * -25 = -200
+    9 * -25 = -225
+    10 * -25 = -250
+    """
+
+    # Optional: use try...except block to handle error conditions
+
+    pass # null operation, placeholder when a statement is required
