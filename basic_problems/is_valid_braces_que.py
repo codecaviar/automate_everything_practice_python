280b96da6d3caf7b79fa245bae3a56cf3811336a
codecaviar <60812709+codecaviar@users.noreply.github.com>
2020-12-19 10:48:05 -0800

    Upload new practice problem

---
+++ basic_problems/is_valid_braces_que.py
@@ -0,0 +1,37 @@
+# Automate Everything from @codecaviar
+# Level: Basic
+# Updated: 2020-12-09
+
+# Fill each function stub according to the docstring
+# Run tests with this command: "python3 -m doctest [filename].py -v"
+
+def is_valid_braces(input_str):
+    """
+    Write a function that takes a string of braces, and determines if
+    the order of the braces is valid. It should return true if the
+    string is valid, and false if it's invalid. All input strings will
+    be nonempty, and will only consist of parentheses, brackets and
+    curly braces: ()[]{}
+
+    Parameters
+    ----------
+    input_str: {str} nonempty string of braces
+
+    Returns
+    -------
+    {bool}: True if a string of braces is considered valid, all braces
+    are matched with the correct brace.
+
+    Example
+    -------
+    >>> is_valid_braces("{}({})[]")
+    True
+    >>> is_valid_braces("())({}}{()][][")
+    False
+    >>> is_valid_braces("(((({{")
+    False
+    """
+
+    # Optional: use try...except block to handle error conditions
+
+    pass # null operation, placeholder when a statement is required
